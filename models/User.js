c7eadfd5bac7fa8ef1442a697c81b163781771be
mlemke24 <mattrlemke@gmail.com>
2021-06-08 14:57:16 -0500

    models done

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: /.+\@.+\..+/,
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('thoughtCount').get(function () {
+    return this.thoughts.reduce(
+        (total, thoughts) => total + thoughts.reactions.length + 1,
+        0
+    );
+});
+
+const User = model('User', UserSchema);
+module.exports = User;
